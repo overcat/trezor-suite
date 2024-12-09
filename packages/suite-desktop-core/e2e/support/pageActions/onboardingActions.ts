c5b661dbfea95071d8762196c33a1d15022fc701
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2024-12-09 14:13:42 +0100

    Feat convert cy pw suite settings (#15796)
    * feat(e2e): Convertin test suite t2b1-deivice-setting to playwright
    
    - deals with disabling hash check on both web and electron
    - enables full configuration of Trezor emulator start from any test
    
    * feat(e2e): Removes converted cy test t2b1-device-settings
    
    * fix(e2e): Fix typing issues and PR feedback
    
    * fix(e2e): Fix homescreen gallery click that is flaky
    
    adds a wait for image load of button that was flaky to click

--- packages/suite-desktop-core/e2e/support/pageActions/onboardingActions.ts
+++ packages/suite-desktop-core/e2e/support/pageActions/onboardingActions.ts
@@ -1,6 +1,13 @@
-import { Locator, Page, expect } from '@playwright/test';
+import { Locator, Page, TestInfo, expect } from '@playwright/test';
+
+import { Model, TrezorUserEnvLink } from '@trezor/trezor-user-env-link';
+import { SUITE as SuiteActions } from '@trezor/suite/src/actions/suite/constants';
+
+import { PlaywrightProjects } from '../../playwright.config';
 
 export class OnboardingActions {
+    readonly model: Model;
+    readonly testInfo: TestInfo;
     readonly welcomeTitle: Locator;
     readonly analyticsHeading: Locator;
     readonly analyticsContinueButton: Locator;
@@ -8,8 +15,17 @@ export class OnboardingActions {
     readonly onboardingViewOnlySkipButton: Locator;
     readonly viewOnlyTooltipGotItButton: Locator;
     readonly connectDevicePrompt: Locator;
+    readonly authenticityStartButton: Locator;
+    readonly authenticityContinueButton: Locator;
+    isModelWithSecureElement = () => ['T2B1', 'T3T1'].includes(this.model);
 
-    constructor(public window: Page) {
+    constructor(
+        public window: Page,
+        model: Model,
+        testInfo: TestInfo,
+    ) {
+        this.model = model;
+        this.testInfo = testInfo;
         this.welcomeTitle = this.window.getByTestId('@welcome/title');
         this.analyticsHeading = this.window.getByTestId('@analytics/consent/heading');
         this.analyticsContinueButton = this.window.getByTestId('@analytics/continue-button');
@@ -17,6 +33,10 @@ export class OnboardingActions {
         this.onboardingViewOnlySkipButton = this.window.getByTestId('@onboarding/viewOnly/skip');
         this.viewOnlyTooltipGotItButton = this.window.getByTestId('@viewOnlyTooltip/gotIt');
         this.connectDevicePrompt = this.window.getByTestId('@connect-device-prompt');
+        this.authenticityStartButton = this.window.getByTestId('@authenticity-check/start-button');
+        this.authenticityContinueButton = this.window.getByTestId(
+            '@authenticity-check/continue-button',
+        );
     }
 
     async optionallyDismissFwHashCheckError() {
@@ -28,10 +48,34 @@ export class OnboardingActions {
     }
 
     async completeOnboarding() {
+        if (this.testInfo.project.name === PlaywrightProjects.Web) {
+            await this.disableFirmwareHashCheck();
+        }
         await this.optionallyDismissFwHashCheckError();
         await this.analyticsContinueButton.click();
         await this.onboardingContinueButton.click();
+        if (this.isModelWithSecureElement()) {
+            await this.authenticityStartButton.click();
+            await TrezorUserEnvLink.pressYes();
+            await this.authenticityContinueButton.click();
+        }
         await this.onboardingViewOnlySkipButton.click();
         await this.viewOnlyTooltipGotItButton.click();
+    }
+
+    async disableFirmwareHashCheck() {
+        // Desktop starts with already disabled firmware hash check. Web needs to disable it.
+        await expect(this.welcomeTitle).toBeVisible({ timeout: 10000 });
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        await this.window.evaluate(SuiteActions => {
+            window.store.dispatch({
+                type: SuiteActions.DEVICE_FIRMWARE_HASH_CHECK,
+                payload: { isDisabled: true },
+            });
+            window.store.dispatch({
+                type: SuiteActions.SET_DEBUG_MODE,
+                payload: { showDebugMenu: true },
+            });
+        }, SuiteActions);
     }
 }
