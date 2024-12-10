8460127e543b736fd17bd2095176ac6320de4701
Daniel Suchý <suchydan@gmail.com>
2024-12-10 14:46:41 +0100

    feat(mobile): add usb priority mode for FW update (#15848)

--- suite-native/app/app.config.ts
+++ suite-native/app/app.config.ts
@@ -172,7 +172,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         slug: appSlugs[buildType],
         owner: appOwners[buildType],
         version: suiteNativeVersion,
-        runtimeVersion: '17',
+        runtimeVersion: '18',
         ...(buildType === 'production'
             ? {}
             : {
