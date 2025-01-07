8db814dca9d3180b69e23404e14afc9437a596ba
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-01-07 12:35:22 +0100

    Feat/suite core/e2e/convert unsupported browsers (#16171)
    * feat(e2e): Convert to pw browser tests
    
    Unfortunatelly, PW does not support Firefox + Mobile combination
    It is also not recommended to run automation on outdated browsers
    
    * fix(e2e): Fix the web visual regresion checks and add it for browser tests
    
    * fix(e2e): add missing browser install on CI
    
    adds also firefox desktop test
    
    * refactor(e2e): switch ios to more stable Aria snapshot verification
    
    also mask larger element for asset visual regression verification
    
    * fix(e2e): Change Safari tests to use Aria comparison too

---
+++ packages/suite-desktop-core/e2e/tests/browser/safari.test.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '../../support/fixtures';
+
+const safariAria = `
+    - heading "Your browser is not supported" [level=1]
+    - paragraph: We recommend using the Trezor Suite desktop app for the best experience. Alternatively, download a supported browser to use the Trezor Suite web app.
+    - link "Desktop App Download":
+      - img
+      - paragraph: Desktop App
+    - link /Chrome \\d+\\+ Download/:
+      - img
+      - paragraph: /Chrome \\d+\\+/
+    - paragraph: Using outdated or unsupported browsers can expose you to security risks. To keep your funds safe, we recommend using the latest version of a supported browser.
+    - paragraph: Continue at my own risk
+`;
+
+test.use({ startEmulator: false, browserName: 'webkit' });
+test.describe('Safari', { tag: ['@group=other', '@webOnly'] }, () => {
+    test('Suite does not support Safari', async ({ page }) => {
+        await expect(page.locator('body')).toMatchAriaSnapshot(safariAria);
+        await expect(page.getByTestId('@continue-to-suite')).toHaveText('Continue at my own risk');
+        await page.getByTestId('@continue-to-suite').click();
+        await expect(page.getByTestId('@welcome/title')).toBeVisible();
+    });
+});
