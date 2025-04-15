1a33eb0bc9d0d92bdf22e2b18861eb85b7ff8849
Jun Luo <4catcode@gmail.com>
2025-04-15 23:11:53 +0800

    fixup! feat(suite): Add basic support for Stellar.

--- packages/suite/src/components/wallet/WalletLayout/AccountBanners/AccountBanners.tsx
+++ packages/suite/src/components/wallet/WalletLayout/AccountBanners/AccountBanners.tsx
@@ -16,11 +16,10 @@ import { BackendDisconnected } from './BackendDisconnected';
 import { ContextMessage } from './ContextMessage';
 import { DeviceUnavailable } from './DeviceUnavailable';
 import { EvmExplanationBanner } from './EvmExplanationBanner';
+import { ReserveBanner } from './ReserveBanner';
 import { StakingBanner } from './StakingBanner';
-import { StellarReserveBanner } from './StellarReserveBanner';
 import { TaprootBanner } from './TaprootBanner';
 import { TorDisconnected } from './TorDisconnected';
-import { XRPReserve } from './XRPReserve';
 
 type AccountBannersProps = {
     account?: Account;
@@ -42,8 +41,7 @@ export const AccountBanners = ({ account }: AccountBannersProps) => {
             <BackendDisconnected />
             <DeviceUnavailable />
             <TorDisconnected />
-            <XRPReserve account={account} />
-            <StellarReserveBanner account={account} />
+            <ReserveBanner account={account} />
             <AccountImported account={account} />
             <AccountOutOfSync account={account} />
             <EvmExplanationBanner account={account} />
